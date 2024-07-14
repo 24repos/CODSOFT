a5aba860a226c072aecf3ff55f039d6b8142aa18
Shanya <shanya.jaidwal@gmail.com>
2024-07-14 18:58:24 +0530

    Innitial commit

---
+++ RandomGame.java
@@ -0,0 +1,45 @@
+import java.util.*;
+ class RandomGame {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int score=0, max=5;
+        boolean play=true;
+
+        while(play) {
+            int attempts=0;
+            boolean guessed=false;
+            int number=(int)(Math.random() * 100)+1;
+            System.out.println("A number has been chosen b/w 1-00. Guess it in 5 attempts!.");
+
+            while(attempts<max && !guessed) {
+                System.out.print("Enter your guess: "); 
+                try {
+                    int user = sc.nextInt();
+                    attempts++;
+                
+                    if (user > number) {
+                        System.out.println("Too high.");
+                    } else if (user < number) {
+                        System.out.println("Too low.");
+                    } else {
+                        System.out.println("Guessed the right number!\n");
+                        score++;
+                        guessed = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error has occurred");
+                    break;
+                }
+                
+            }
+            if(!guessed) {
+                System.out.println("\nOut of attempts. The number was "+ number);
+            }
+            System.out.print("Do you want to play again? (yes/no): ");
+            String response= sc.next();
+            play=response.equalsIgnoreCase("yes");
+        }
+        sc.close();
+        System.out.println("\nTotal Score = "+score+"\nThankyou for playing!");
+    }
+}
