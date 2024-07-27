abd833cc8af1a4da68e4e7d6f05b88ea67db2106
Shanya <shanya.jaidwal@gmail.com>
2024-07-27 16:30:26 +0530

    Improved the game by handling the possible edge cases.

--- RandomGame.java
+++ RandomGame.java
@@ -1,30 +1,42 @@
 import java.util.*;
- class RandomGame {
+class RandomGame {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int score=0, max=5;
         boolean play=true;
+        int gamecount = 0;
+        int wrongInput=0;
 
         while(play) {
             int attempts=0;
             boolean guessed=false;
             int number=(int)(Math.random() * 100)+1;
-            System.out.println("A number has been chosen b/w 1-00. Guess it in 5 attempts!.");
-
+            System.out.println("A number has been chosen b/w 100. Guess it in 5 attempts!.");
+            
             while(attempts<max && !guessed) {
                 System.out.print("Enter your guess: "); 
                 try {
                     int user = sc.nextInt();
-                    attempts++;
-                
-                    if (user > number) {
-                        System.out.println("Too high.");
-                    } else if (user < number) {
-                        System.out.println("Too low.");
-                    } else {
-                        System.out.println("Guessed the right number!\n");
-                        score++;
-                        guessed = true;
+
+                    if((user>100 || user<1)) {
+                        System.out.println("Please enter a number between 1-100");
+                        wrongInput++;
+                        if(wrongInput == 3){
+                            System.out.println("Too many wrong inputs");
+                            break;
+                        }
+                    }
+                    else {
+                        attempts++;
+                        if (user > number) {
+                            System.out.println("Too high.");
+                        } else if (user < number) {
+                            System.out.println("Too low.");
+                        } else {
+                            System.out.println("Guessed the right number!\n");
+                            score++;
+                            guessed = true;
+                        }
                     }
                 } catch (InputMismatchException e) {
                     System.out.println("Input must be an integer. Please try again.");
@@ -38,8 +50,9 @@ import java.util.*;
             System.out.print("Do you want to play again? (yes/no): ");
             String response= sc.next();
             play=response.equalsIgnoreCase("yes");
+            gamecount+=1;
         }
         sc.close();
-        System.out.println("\nTotal Score = "+score+"\nThankyou for playing!");
+        System.out.println("\nTotal Score = "+score+"/"+gamecount+"\nThankyou for playing!");
     }
 }
